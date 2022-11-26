5d9b58f57f9cd75b3ec3207a0bfa3eb10ffa5a55
Belfagor2005 <72314956+Belfagor2005@users.noreply.github.com>
2022-11-26 12:33:58 +0100

    Add files via upload

---
+++ README.md
@@ -0,0 +1,11 @@
+Multimedia Addon 
+
+Enigma2 project
+MerlinMusicPlayer
+
+author Dr.Best
+Merlin Music Player
+Merlin Music Player and iDream
+Manage your music files in a database, play it with Merlin Music Player.
+http://www.dreamboxupdate.com/preview/plugin_merlinmusicplayer.jpg	    
+
---
+++ usr/lib/enigma2/python/Plugins/Extensions/MerlinMusicPlayer/__init__.py
@@ -0,0 +1,22 @@
+from Components.Language import language
+from Tools.Directories import resolveFilename, SCOPE_PLUGINS
+import gettext
+
+PluginLanguageDomain = "MerlinMusicPlayer"
+PluginLanguagePath = "Extensions/MerlinMusicPlayer/locale"
+
+
+def localeInit():
+	gettext.bindtextdomain(PluginLanguageDomain, resolveFilename(SCOPE_PLUGINS, PluginLanguagePath))
+
+
+def _(txt):
+	if gettext.dgettext(PluginLanguageDomain, txt):
+		return gettext.dgettext(PluginLanguageDomain, txt)
+	else:
+		#print "[" + PluginLanguageDomain + "] fallback to default translation for " + txt
+		return gettext.gettext(txt)
+
+
+localeInit()
+language.addCallback(localeInit)
